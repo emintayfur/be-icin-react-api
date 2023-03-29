5998e42e4cca00b744ad2ada8ae5a8443cad7040
Emin <emintayfur@icloud.com>
2023-03-30 00:59:04 +0300

    4 saniyelik gecikme eklendi, 404 response'u değiştirildi.

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 const port = 3001;
 let list = [];
 
-const express = require('express')
+const express = require("express");
 const app = express();
 
-// app.use(require('cors')());
+app.use(require("cors")());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,53 +14,82 @@ app.use(express.urlencoded({ extended: false }));
 const successResponse = (data) => ({
     status: true,
     data,
-})
+});
 
 const errorResponse = (message = null) => ({
     status: true,
     message,
-})
-
-// Listele
-app.get('/todos', (req, res) => {
-    res.json(successResponse(list));
-})
-
-// Ekle
-app.post('/todo', (req, res) => {
-    const todo = req?.body?.str;
-
-    if (!todo || typeof todo !== 'string') {
-        return res.status(400).json(errorResponse('Lütfen geçerli bir değer gönderin!'))
-    }
-
-    const foundItem = list.find((item) => item.toLowerCase() === todo.toLowerCase());
-    if (foundItem) {
-        return res.status(409).json(errorResponse(`"${todo}" zaten mevcut!`))
-    }
-
-    list.push(todo);
-    res.status(201).json(successResponse())
 });
 
+// 4 saniyelik gecikme
+const initialAppDelay = 4000;
+const withDelay =
+    (fc, ms = initialAppDelay) =>
+        (req, res) => {
+            setTimeout(() => fc(req, res), ms);
+            return null;
+        };
+
+// Listele
+app.get(
+    "/todos",
+    withDelay((req, res) => {
+        res.json(successResponse(list));
+    })
+);
+
+// Ekle
+app.post(
+    "/todo",
+    withDelay((req, res) => {
+        const todo = req?.body?.str;
+
+        if (!todo || typeof todo !== "string") {
+            return res
+                .status(400)
+                .json(errorResponse("Lütfen geçerli bir değer gönderin!"));
+        }
+
+        const foundItem = list.find(
+            (item) => item.toLowerCase() === todo.toLowerCase()
+        );
+        if (foundItem) {
+            return res.status(409).json(errorResponse(`"${todo}" zaten mevcut!`));
+        }
+
+        list.push(todo);
+        res.status(201).json(successResponse());
+    })
+);
+
 // Sil
-app.delete('/todo', (req, res) => {
-    const index = req?.body?.index;
+app.delete(
+    "/todo",
+    withDelay((req, res) => {
+        const index = req?.body?.index;
 
-    if (typeof index !== 'number') {
-        return res.status(400).json(errorResponse('Lütfen geçerli bir değer gönderin!'))
-    }
+        if (typeof index !== "number") {
+            return res
+                .status(400)
+                .json(errorResponse("Lütfen geçerli bir değer gönderin!"));
+        }
 
-    const foundItem = list?.[index];
-    if (!foundItem) {
-        return res.status(404).json(errorResponse('Bulunamadı!'))
-    }
+        const foundItem = list?.[index];
+        if (!foundItem) {
+            return res.status(404).json(errorResponse("Bulunamadı!"));
+        }
 
-    delete list[index];
-    list = list.filter(Boolean);
-    return res.json(successResponse())
-})
+        delete list[index];
+        list = list.filter(Boolean);
+        return res.json(successResponse());
+    })
+);
+
+app.use((req, res, next) => {
+    res.status(404).json(errorResponse("Not found"));
+    next();
+});
 
 app.listen(port, () => {
-    console.log(`Sunucu "${port}" portu üstünde çalışmaya başladı :)`)
-})
+    console.log(`Sunucu "${port}" portu üstünde çalışmaya başladı :)`);
+});
